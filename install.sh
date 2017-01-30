5e5408d8948af15ad593aca6e9b9b93cad2f27f2
Paul H <paul@frozenphoenix.net>
2017-01-29 19:02:18 -0500

    pi install script

--- install.sh
+++ install.sh
@@ -30,5 +30,24 @@ sudo dpkg -i libttspico-utils_1.0+git20110131-2_armhf.deb
 cd svox-1.0+git20110131
 dpkg-buildpackage -rfakeroot -us -uc
 
+cd ..
 echo 'Installing pocketsphinx'
 sudo apt-get install pocketsphinx
+
+sudo apt-get install subversion autoconf libtool automake gfortran g++ --yes
+
+svn co https://svn.code.sf.net/p/cmusphinx/code/trunk/cmuclmtk/
+cd cmuclmtk/
+./autogen.sh && make && sudo make install
+cd ..
+
+sudo su -c "echo 'deb http://ftp.debian.org/debian experimental main contrib non-free' > /etc/apt/sources.list.d/experimental.list"
+sudo apt-get update
+sudo apt-get -t experimental install phonetisaurus m2m-aligner mitlm libfst-tools
+
+wget https://www.dropbox.com/s/kfht75czdwucni1/g014b2b.tgz
+tar -xvf g014b2b.tgz
+cd g014b2b/
+./compile-fst.sh
+cd ..
+mv ~/g014b2b ~/phonetisaurus
